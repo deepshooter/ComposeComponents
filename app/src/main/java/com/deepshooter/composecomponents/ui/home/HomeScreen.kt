efe83bdd2d2962f4ec07bc551200341e89c460b8
avinashmishra <avinashm720@gmail.com>
2023-07-23 08:48:53 +0530

    Design Home Screen

--- app/src/main/java/com/deepshooter/composecomponents/ui/home/HomeScreen.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/home/HomeScreen.kt
@@ -1,8 +1,54 @@
 package com.deepshooter.composecomponents.ui.home
 
+import android.content.res.Configuration
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.deepshooter.composecomponents.R
 import com.deepshooter.composecomponents.ui.Screen
+import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
 import com.deepshooter.composecomponents.ui.theme.TailwindCSSColor
+import com.deepshooter.composecomponents.utils.shadow
 
 
 private val menuItems = listOf(
@@ -31,3 +77,169 @@ private val menuItems = listOf(
         route = Screen.Tutorials
     )
 )
+
+@Composable
+fun HomeScreen(
+    navigate: (Screen) -> Unit = {},
+    turnOnDarkMode: (Boolean) -> Unit = {}
+) {
+
+
+    val isDark = isSystemInDarkTheme()
+    val (darkModeState, onDarkModeStateChange) = remember { mutableStateOf(isDark) }
+
+    Scaffold(
+        Modifier
+            .navigationBarsPadding()
+            .imePadding()
+    ) { innerPadding ->
+
+        Column(
+            Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+
+            Box(
+                modifier = Modifier.weight(1f)
+            ) {
+                LazyVerticalGrid(
+                    modifier = Modifier.fillMaxSize(),
+                    columns = GridCells.Fixed(2),
+                    contentPadding = PaddingValues(24.dp, 8.dp, 24.dp, 24.dp)
+                ) {
+                    item(span = { GridItemSpan(maxCurrentLineSpan) }) {
+
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(Modifier.windowInsetsTopHeight(WindowInsets.statusBars))
+
+                            Text(
+                                modifier = Modifier
+                                    .padding(
+                                        start = 16.dp,
+                                        top = 32.dp,
+                                        end = 16.dp,
+                                        bottom = 16.dp
+                                    )
+                                    .fillMaxWidth(),
+                                text = stringResource(id = R.string.app_name),
+                                style = MaterialTheme.typography.h5,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+
+                    item {
+                        ModuleButton(
+                            name = if (isDark) "Dark Mode" else "Light Mode",
+                            icon = if (isDark) {
+                                R.drawable.ic_moon_stars
+                            } else {
+                                R.drawable.ic_brightness_high
+                            },
+                            color = TailwindCSSColor.Green500,
+                            onClick = {
+                                onDarkModeStateChange(!darkModeState)
+                                turnOnDarkMode(!darkModeState)
+                            }
+                        )
+                    }
+
+                    items(menuItems) { menu ->
+                        ModuleButton(
+                            name = menu.name,
+                            icon = menu.icon,
+                            color = menu.color,
+                            onClick = {
+                                navigate(menu.route)
+                            }
+                        )
+                    }
+                }
+
+                Box(
+                    Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .height(16.dp)
+                        .background(
+                            Brush.verticalGradient(
+                                0f to Color.Transparent,
+                                1f to MaterialTheme.colors.background
+                            )
+                        )
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ModuleButton(
+    name: String,
+    @DrawableRes icon: Int,
+    color: Color,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier
+            .padding(0.dp)
+            .fillMaxWidth()
+            .shadow(
+                spread = 8.dp,
+                alpha = .25f,
+                color = color,
+                radius = 8.dp,
+            ),
+        shape = MaterialTheme.shapes.medium,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            contentColor = color
+        ),
+        onClick = onClick,
+        contentPadding = PaddingValues(8.dp),
+        elevation = null
+    ) {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                modifier = Modifier.size(32.dp),
+                painter = painterResource(id = icon),
+                contentDescription = name,
+                tint = LocalContentColor.current
+            )
+            Text(
+                modifier = Modifier.padding(top = 8.dp),
+                text = name,
+                color = LocalContentColor.current,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeIndexScreenPreview() {
+    ComposeComponentsTheme {
+        HomeScreen()
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HomeIndexScreenPreviewDark() {
+    ComposeComponentsTheme {
+        HomeScreen()
+    }
+}
