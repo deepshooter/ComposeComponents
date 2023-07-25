2a2feb76fbf73b76c5745c522a666708c7d9dee3
avinashmishra <avinashm720@gmail.com>
2023-07-25 18:52:47 +0530

    Change In Theme

--- app/src/main/java/com/deepshooter/composecomponents/ui/theme/Theme.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/theme/Theme.kt
@@ -1,66 +1,65 @@
 package com.deepshooter.composecomponents.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = TailwindCSSColor.Green700,
+    //primaryVariant = TailwindCSSColor.Green900,
+    secondary = TailwindCSSColor.Pink700,
+    //secondaryVariant = TailwindCSSColor.Pink900,
+    background = TailwindCSSColor.Gray900,
+    surface = Color.Black,
+    error = TailwindCSSColor.Red700,
+
+    onPrimary = TailwindCSSColor.Gray50,
+    onSecondary = TailwindCSSColor.Gray50,
+    onBackground = TailwindCSSColor.Gray50,
+    onSurface = TailwindCSSColor.Gray50
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = TailwindCSSColor.Green500,
+    //primaryVariant = TailwindCSSColor.Green700,
+    secondary = TailwindCSSColor.Pink500,
+    //secondaryVariant = TailwindCSSColor.Pink700,
+    background = TailwindCSSColor.Gray50,
+    surface = Color.White,
+    error = TailwindCSSColor.Red500,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onPrimary = TailwindCSSColor.Gray50,
+    onSecondary = TailwindCSSColor.Gray50,
+    onBackground = TailwindCSSColor.Gray900,
+    onSurface = TailwindCSSColor.Gray900
 )
 
 @Composable
 fun ComposeComponentsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = !darkTheme
+        )
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
     }
+
 
     MaterialTheme(
         colorScheme = colorScheme,
