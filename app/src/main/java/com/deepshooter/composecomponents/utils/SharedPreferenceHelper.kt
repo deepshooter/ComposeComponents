5e45206c3bd26c8037a6bf00332d364f328dc304
avinashmishra <avinashm720@gmail.com>
2023-07-25 08:24:35 +0530

    Add SharedPreferenceHelper Class

---
+++ app/src/main/java/com/deepshooter/composecomponents/utils/SharedPreferenceHelper.kt
@@ -0,0 +1,45 @@
+package com.deepshooter.composecomponents.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class SharedPreferenceHelper @Inject constructor(@ApplicationContext context: Context) {
+
+    private val context: Context = context.applicationContext
+
+    @Volatile
+    private var sharedPreferences: SharedPreferences? = null
+
+    private fun getSharedPreferences(): SharedPreferences {
+        return sharedPreferences ?: synchronized(this) {
+            context.getSharedPreferences(
+                "${context.packageName}.main",
+                Context.MODE_PRIVATE
+            )
+        }
+    }
+
+    fun clearSharedPreferences() {
+        getSharedPreferences().edit().clear().apply()
+    }
+
+    fun setDarkMode(isDark: Boolean) {
+        getSharedPreferences()
+            .edit()
+            .apply {
+                putBoolean(PREF_DARK_MODE, isDark)
+                apply()
+            }
+    }
+
+    fun getDarkMode(): Boolean {
+        return getSharedPreferences().getBoolean(PREF_DARK_MODE, false)
+    }
+
+    companion object {
+        private const val PREF_DARK_MODE = "dark_mode"
+    }
+
+}
