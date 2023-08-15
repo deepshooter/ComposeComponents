8b234f34dcbeca0c0e39ffc6301619e7e54444e5
avinashmishra <avinashm720@gmail.com>
2023-08-15 09:34:38 +0530

    Modified AppBar Background

--- app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/appbar/AppBarScreen.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/appbar/AppBarScreen.kt
@@ -15,20 +15,27 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
+import com.deepshooter.composecomponents.ui.theme.Purple200
 import com.deepshooter.composecomponents.utils.AppComponent
 import com.deepshooter.composecomponents.utils.AppComponent.Header
+import com.deepshooter.composecomponents.utils.UIThemeController
 
 @Composable
 fun AppBarScreen(
@@ -74,6 +81,10 @@ fun AppBarScreenSkeleton(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SimpleTopAppBar() {
+
+    val isDark by UIThemeController.isDarkMode.collectAsState()
+
+
     TopAppBar(
         title = { Text("Simple TopAppBar") },
         navigationIcon = {
@@ -89,13 +100,22 @@ fun SimpleTopAppBar() {
             IconButton(onClick = { /* doSomething() */ }) {
                 Icon(Icons.Filled.Favorite, contentDescription = "Description")
             }
-        }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = if (isDark) Color.Black else Purple200,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onSecondary
+        )
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FlexibleTopAppBar() {
+
+    val isDark by UIThemeController.isDarkMode.collectAsState()
+
     TopAppBar(title = {
         Text(
             modifier = Modifier.fillMaxWidth(),
@@ -104,12 +124,21 @@ fun FlexibleTopAppBar() {
             fontWeight = FontWeight.Bold,
             fontSize = 24.sp
         )
-    })
+    },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = if (isDark) Color.Black else Purple200,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onSecondary
+        )
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTopAppBar() {
+
+    val isDark by UIThemeController.isDarkMode.collectAsState()
 
     TopAppBar(
         modifier = Modifier.clip(RoundedCornerShape(0.dp, 0.dp, 24.dp, 24.dp)),
@@ -121,7 +150,14 @@ fun CustomTopAppBar() {
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp
             )
-        })
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = if (isDark) Color.Black else Purple200,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onSecondary
+        )
+    )
 }
 
 @Preview
