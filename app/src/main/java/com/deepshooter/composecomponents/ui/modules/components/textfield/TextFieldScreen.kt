84c3b469da0fb471fe9cf9dbee75c2c3fc4f786c
avinashmishra <avinashm720@gmail.com>
2023-08-20 19:38:27 +0530

    Add TextFields in TextFieldScreen

--- app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/textfield/TextFieldScreen.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/textfield/TextFieldScreen.kt
@@ -2,6 +2,7 @@ package com.deepshooter.composecomponents.ui.modules.components.textfield
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,12 +11,14 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,6 +29,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
@@ -76,6 +81,12 @@ fun TextFieldScreenSkeleton(
                 AppComponent.MediumSpacer()
                 TextFieldWithIcons()
                 AppComponent.MediumSpacer()
+                TextFieldWithPlaceholder()
+                AppComponent.MediumSpacer()
+                TextFieldWithErrorState()
+                AppComponent.MediumSpacer()
+                TextFieldWithHelperMessage()
+                AppComponent.MediumSpacer()
 
             }
 
@@ -122,6 +133,72 @@ fun TextFieldWithIcons() {
         leadingIcon = { Icon(Icons.Filled.Favorite, contentDescription = "Localized description") },
         trailingIcon = { Icon(Icons.Filled.Info, contentDescription = "Localized description") }
     )
+}
+
+@Composable
+fun TextFieldWithPlaceholder() {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Email") },
+        placeholder = { Text("example@gmail.com") }
+    )
+}
+
+@Composable
+fun TextFieldWithErrorState() {
+    val context = LocalContext.current
+    var text by rememberSaveable { mutableStateOf("") }
+    var isError by rememberSaveable { mutableStateOf(false) }
+
+    fun validate(text: String) {
+        isError = text.count() < 5
+    }
+
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+            isError = false
+        },
+        singleLine = true,
+        label = { Text(if (isError) "Email*" else "Email") },
+        isError = isError,
+        keyboardActions = KeyboardActions { validate(text) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .semantics {
+                // Provide localized description of the error
+                if (isError) {
+                    Toast
+                        .makeText(context, "Email format is invalid.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+    )
+}
+
+@Composable
+fun TextFieldWithHelperMessage() {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    Column {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Label") }
+        )
+        Text(
+            text = "Helper message",
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
 }
 
 @Preview
