e98d6ca7189439bc318915e6d4869d8dfd53938e
avinashmishra <avinashm720@gmail.com>
2023-08-27 13:37:57 +0530

    Add Button Screen

---
+++ app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/button/ButtonScreen.kt
@@ -0,0 +1,67 @@
+package com.deepshooter.composecomponents.ui.modules.components.button
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
+import com.deepshooter.composecomponents.utils.AppComponent
+
+
+@Composable
+fun ButtonScreen(
+    goBack: () -> Unit
+) {
+    ButtonScreenSkeleton(
+        goBack = goBack
+    )
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun ButtonScreenSkeleton(
+    goBack: () -> Unit = {}
+) {
+    Scaffold(
+        Modifier
+            .navigationBarsPadding()
+            .imePadding()
+            .statusBarsPadding()
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            AppComponent.Header(
+                "Button",
+                goBack = goBack
+            )
+
+            Divider()
+
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+fun ButtonScreenSkeletonPreview() {
+    ComposeComponentsTheme() {
+        ButtonScreenSkeleton()
+    }
+}
