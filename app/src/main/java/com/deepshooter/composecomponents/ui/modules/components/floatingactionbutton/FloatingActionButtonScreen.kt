80b03f19eca3cc435acaf60bc9291f716b3bbd16
avinashmishra <avinashm720@gmail.com>
2023-09-07 07:31:58 +0530

    Add FloatingActionButtons in FloatingActionButtonScreen

--- app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/floatingactionbutton/FloatingActionButtonScreen.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/floatingactionbutton/FloatingActionButtonScreen.kt
@@ -4,16 +4,28 @@ import android.annotation.SuppressLint
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.deepshooter.composecomponents.R
 import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
 import com.deepshooter.composecomponents.utils.AppComponent
 import com.deepshooter.composecomponents.utils.AppConstant.FLOATING_ACTION_BUTTON
@@ -52,6 +64,46 @@ fun FloatingActionButtonScreenSkeleton(
 
             Divider()
 
+            Column(
+                Modifier.padding(start = 16.dp, end = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AppComponent.MediumSpacer()
+
+                FloatingActionButton(onClick = { /*do something*/ }) {
+                    Icon(
+                        Icons.Filled.Favorite,
+                        contentDescription = stringResource(id = R.string.localized_description)
+                    )
+                }
+
+                AppComponent.MediumSpacer()
+
+                ExtendedFloatingActionButton(
+                    content = { Text(stringResource(R.string.extended)) },
+                    onClick = {}
+                )
+
+                AppComponent.MediumSpacer()
+
+                ExtendedFloatingActionButton(
+                    icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                    text = { Text(stringResource(R.string.add_to_basket)) },
+                    onClick = { /*do something*/ }
+                )
+
+                AppComponent.MediumSpacer()
+
+                ExtendedFloatingActionButton(
+                    icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                    text = { Text(stringResource(R.string.fluid_fab)) },
+                    onClick = { /*do something*/ },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+            }
+
+            AppComponent.BigSpacer()
         }
     }
 }
@@ -60,7 +112,7 @@ fun FloatingActionButtonScreenSkeleton(
 @Preview
 @Composable
 fun FloatingActionButtonScreenSkeletonPreview() {
-    ComposeComponentsTheme() {
+    ComposeComponentsTheme {
         FloatingActionButtonScreenSkeleton()
     }
 }
