90a8fffd1999f56cbb8da8d09b9476120c58071d
avinashmishra <avinashm720@gmail.com>
2023-09-11 18:49:58 +0530

    Add Slider in SliderScreen

--- app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/slider/SliderScreen.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/slider/SliderScreen.kt
@@ -6,14 +6,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.deepshooter.composecomponents.R
 import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
 import com.deepshooter.composecomponents.utils.AppComponent
 import com.deepshooter.composecomponents.utils.AppConstant.SLIDER
@@ -52,6 +64,38 @@ fun SliderScreenSkeleton(
             )
 
             Divider()
+
+            Column(Modifier.padding(start = 16.dp, end = 16.dp)) {
+
+                AppComponent.MediumSpacer()
+
+                var sliderPosition by remember { mutableFloatStateOf(0f) }
+                Text(text = String.format(stringResource(R.string._2f), sliderPosition))
+                Slider(value = sliderPosition, onValueChange = { sliderPosition = it })
+
+                AppComponent.MediumSpacer()
+
+                var stepSliderPosition by remember { mutableFloatStateOf(0f) }
+                Text(text = String.format(stringResource(R.string._2f), stepSliderPosition))
+                Slider(
+                    value = stepSliderPosition,
+                    onValueChange = { stepSliderPosition = it },
+                    valueRange = 0f..100f,
+                    onValueChangeFinished = {
+                        // launch some business logic update with the state you hold
+                        // viewModel.updateSelectedSliderValue(sliderPosition)
+                    },
+                    steps = 5,
+                    colors = SliderDefaults.colors(
+                        thumbColor = MaterialTheme.colorScheme.secondary,
+                        activeTrackColor = MaterialTheme.colorScheme.secondary
+                    )
+                )
+
+            }
+
+            AppComponent.BigSpacer()
+
 
 
         }
