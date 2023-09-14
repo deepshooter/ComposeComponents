59a4ffa3a8efe7224adb23ea4807fc82a9b70a6a
avinashmishra <avinashm720@gmail.com>
2023-09-14 19:04:54 +0530

    Add SimpleMenu in DropDownMenuScreen

--- app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/dropdownmenu/DropDownMenuScreen.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/dropdownmenu/DropDownMenuScreen.kt
@@ -2,17 +2,34 @@ package com.deepshooter.composecomponents.ui.modules.components.dropdownmenu
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.deepshooter.composecomponents.R
 import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
 import com.deepshooter.composecomponents.utils.AppComponent
 import com.deepshooter.composecomponents.utils.AppConstant.DROPDOWN_MENU
@@ -52,11 +69,63 @@ fun DropDownMenuScreenSkeleton(
 
             Divider()
 
+            AppComponent.SubHeader(stringResource(R.string.menu))
+
+            SimpleMenu()
+
 
         }
 
     }
 
+}
+
+@Composable
+fun SimpleMenu() {
+
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopCenter)
+    ) {
+
+        IconButton(onClick = { expanded = true }) {
+            Icon(
+                Icons.Default.MoreVert,
+                contentDescription = stringResource(R.string.localized_description)
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.refresh)) },
+                onClick = {
+                    expanded = false
+                    /* Handle refresh! */
+                })
+
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.settings)) },
+                onClick = {
+                    expanded = false
+                    /* Handle settings! */
+                })
+
+            Divider()
+
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.send_feedback)) },
+                onClick = {
+                    expanded = false
+                    /* Handle send feedback! */
+                })
+        }
+    }
 }
 
 
