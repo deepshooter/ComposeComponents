fdc09d7dd11b032fb7defe91ba7671add00c0cd7
avinashmishra <avinashm720@gmail.com>
2023-09-26 08:02:18 +0530

    Code CleanUp

--- app/src/main/java/com/deepshooter/composecomponents/models/ListItem.kt
+++
@@ -1,7 +0,0 @@
-package com.deepshooter.composecomponents.models
-
-data class ListItem(
-    val id: Int,
-    val name: String,
-    val image: String = "https://picsum.photos/seed/$id/128/128"
-)
--- app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/swiperefresh/SwipeRefreshScreen.kt
+++ app/src/main/java/com/deepshooter/composecomponents/ui/modules/components/swiperefresh/SwipeRefreshScreen.kt
@@ -2,64 +2,35 @@ package com.deepshooter.composecomponents.ui.modules.components.swiperefresh
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.deepshooter.composecomponents.models.ListItem
 import com.deepshooter.composecomponents.ui.theme.ComposeComponentsTheme
 import com.deepshooter.composecomponents.utils.AppComponent
 import com.deepshooter.composecomponents.utils.AppConstant.SWIPE_REFRESH
-import com.deepshooter.composecomponents.utils.MockData
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-
 
 @Composable
 fun SwipeRefreshScreen(
     goBack: () -> Unit
 ) {
 
-    val scope = rememberCoroutineScope()
+    SwipeRefreshScreenSkeleton(goBack = goBack)
 
-    val items = remember { mutableStateOf(MockData.dummyListItem) }
-
-    val isRefreshing = remember { mutableStateOf(false) }
-
-    SwipeRefreshScreenSkeleton(
-        goBack = goBack,
-        items = items.value,
-        isRefreshing = isRefreshing.value,
-        onRefresh = {
-            scope.launch {
-                isRefreshing.value = true
-
-                delay(2000)
-
-                items.value = items.value.shuffled()
-
-                isRefreshing.value = false
-            }
-        }
-    )
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SwipeRefreshScreenSkeleton(
     goBack: () -> Unit = {},
-    items: List<ListItem>,
-    isRefreshing: Boolean = false,
-    onRefresh: () -> Unit = {}
 ) {
 
     Scaffold(
@@ -79,6 +50,14 @@ fun SwipeRefreshScreenSkeleton(
 
             Divider()
 
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+
+            }
+
         }
     }
 }
@@ -87,11 +66,7 @@ fun SwipeRefreshScreenSkeleton(
 @Composable
 fun SwipeRefreshScreenSkeletonPreview() {
     ComposeComponentsTheme {
-        val items = remember { mutableStateOf(MockData.dummyListItem) }
-
-        SwipeRefreshScreenSkeleton(
-            items = items.value
-        )
+        SwipeRefreshScreenSkeleton()
     }
 }
 
@@ -99,10 +74,6 @@ fun SwipeRefreshScreenSkeletonPreview() {
 @Composable
 fun SwipeRefreshScreenSkeletonPreviewDark() {
     ComposeComponentsTheme {
-        val items = remember { mutableStateOf(MockData.dummyListItem) }
-
-        SwipeRefreshScreenSkeleton(
-            items = items.value
-        )
+        SwipeRefreshScreenSkeleton()
     }
 }
